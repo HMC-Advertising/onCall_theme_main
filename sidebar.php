6e9e4325eeb38900e3a313dadaa4947f609331cc
amanda iaria <amanda.iaria@gmail.com>
2014-10-16 17:32:06 -0400

    sidebar

---
+++ sidebar.php
@@ -0,0 +1,10 @@
+<section id="sidebar" class="col-lg-4">
+	
+	<h1>Ask a Question</h1>
+	Gravity Forms section
+	<?php
+		if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('main sidebar')):
+		endif;
+	?>
+
+</section>
